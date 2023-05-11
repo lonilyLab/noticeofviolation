28c085da5a0c748145e7e1c4a5bec6e336933ed4
lonily <lie386@newschool.edu>
2023-05-11 06:37:41 -0400

    Changes

--- script.js
+++ script.js
@@ -24,8 +24,9 @@ fetch(url)
         <div class="labor-item">
         <div class="labor-topic">${labor.topic}</div>
         <div class="labor-outcome">${labor.outcome}</div>
-        <div class="labor-info">${labor.request_type}<br>
-        ${labor.language}, ${formatDate(labor.intake_date)}</div>
+        <div class="labor-info">${labor.language}<br>
+        ${labor.request_type}<br>
+        ${formatDate(labor.intake_date)}</div>
         </div>`;
       });
       laborContainer.innerHTML = laborDOM;
@@ -47,3 +48,47 @@ var i;
       }
     });
   }
+
+  var selects = Array.from(document.getElementsByClassName("selector")).map(
+    (selector)=>selector.getElementsByTagName("select")[0])
+  
+  
+  for (var j = 0; j < selects.length; j++) {
+    let select = selects[j]
+    select.addEventListener("change", function(){
+        console.log(select.value)
+        // do something after selecting dropdown
+      }
+    )
+  }
+  
+var submit = document.getElementsByClassName("submit")[0]
+  submit.addEventListener("click", function () {
+    laborStuff = document.getElementsByClassName("labor-item")
+    console.log(laborStuff)
+    for (var k=0; k < laborStuff.length; k++){
+      laborEntry = laborStuff[k]
+      display = []
+      for (var j=0; j < selects.length; j++){
+        if (selects[j].value != "None"){
+          if (selects[j].name == 'topic'){ // Filter by topic
+            var text = laborEntry.getElementsByClassName("labor-topic")[0].textContent
+            display.push(text == selects[j].value)
+          } else if (selects[j].name == 'requesttype'){ // Filter by request type
+            var text = laborEntry.getElementsByClassName("labor-info")[0].textContent.split('\n')[0]
+            display.push(text == selects[j].value)
+          } else if (selects[j].name == 'outcome'){ // Filter by outcome
+            var text = laborEntry.getElementsByClassName("labor-outcome")[0].textContent
+            display.push(text == selects[j].value)
+          }
+        } else {
+          display.push(true)
+        }
+      }
+      if (display.every(Boolean)){ // display if all values in display are true
+        laborEntry.style.display = 'flex'
+      } else {
+        laborEntry.style.display = 'none'
+      }
+    }
+  })
